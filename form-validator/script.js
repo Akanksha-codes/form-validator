cb1fe86211668da434039c526a09780ffe7c53c8
Eng-Akanksha <73644135+Eng-Akanksha@users.noreply.github.com>
2020-10-30 11:46:18 +0530

    Update script.js

--- form-validator/script.js
+++ form-validator/script.js
@@ -74,5 +74,13 @@ function getFieldName(input) {
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
+   if(!checkRequired([username, email, password, password2])){
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+  }
+
+});
 
 
